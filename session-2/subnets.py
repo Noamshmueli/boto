8573826e8c7ad4d2e0f2f0258f26e7ee1ce07ac7
Noamshmueli <47123652+Noamshmueli@users.noreply.github.com>
2019-04-16 20:34:19 +0300

    Update subnets.py

--- session-2/subnets.py
+++ session-2/subnets.py
@@ -19,3 +19,19 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    
+def main():
+    parser = parsing.Parsing()
+    args = parser.args_parser()
+    aws = awsops.AwsOperations(args)
+    public_cidr = config.MAPPING[config.PRIVATE_TAG]
+    subnet_id = aws.create_subnet(public_cidr)
+    aws.create_tags(subnet_id, config.PRIVATE_TAG)
+    # get private subnet cidr	
+    # create private subnet
+    # tag the private subnet
+
+
+if __name__ == '__main__':
+    main()
